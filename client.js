83bc5479371e44dbf63501af99aca77ffb4d9350
Matheus Marques <matheus_marquest@outlook.com>
2025-01-06 12:57:53 -0300

    first commit

---
+++ client.js
@@ -0,0 +1,29 @@
+window.TrelloPowerUp.initialize({
+  'card-buttons': function (t) {
+    return [{
+      icon: 'https://exemplo.com/icone.png',
+      text: 'Definir Estimativa',
+      callback: function (t) {
+        return t.popup({
+          title: 'Estimativa de Desenvolvimento',
+          url: 'popup.html',
+          height: 200
+        });
+      }
+    }];
+  },
+
+  'card-badges': function (t) {
+    return t.get('card', 'shared', 'estimativa')
+      .then(function (estimativa) {
+        if (estimativa) {
+          return [{
+            text: `${estimativa} horas`,
+            color: 'blue'
+          }];
+        } else {
+          return [];
+        }
+      });
+  }
+});
