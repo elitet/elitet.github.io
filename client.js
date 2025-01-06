ab03f938a4544c3297777db9ec510be46e32a4fc
Matheus Marques <matheus_marquest@outlook.com>
2025-01-06 15:29:48 -0300

    Many updates

--- client.js
+++ client.js
@@ -1,25 +1,27 @@
 window.TrelloPowerUp.initialize({
+  // Botão no card
   'card-buttons': function (t) {
     return [{
-      icon: 'https://cdn-icons-png.flaticon.com/512/1782/1782440.png',
+      icon: 'https://cdn-icons-png.flaticon.com/512/833/833604.png',
       text: 'Definir Estimativa',
       callback: function (t) {
         return t.popup({
           title: 'Estimativa de Desenvolvimento',
           url: 'popup.html',
-          height: 200
+          height: 220
         });
       }
     }];
   },
 
+  // Badge que aparece no card
   'card-badges': function (t) {
     return t.get('card', 'shared', 'estimativa')
       .then(function (estimativa) {
         if (estimativa) {
           return [{
-            text: `${estimativa} horas`,
-            color: 'blue'
+            icon: "https://cdn-icons-png.flaticon.com/512/833/833604.png",
+            text: estimativa
           }];
         } else {
           return [];
